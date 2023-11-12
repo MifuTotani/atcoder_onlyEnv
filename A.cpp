f5c7981d857e45da822fcd0fa54902e643515d8e
MifuTotani <m.totani.861@nitech.jp>
2023-11-12 10:47:42 +0900

    rev_A.cpp

--- A.cpp
+++ A.cpp
@@ -6,6 +6,8 @@ using ll = long long;
 
 int main() {
   // cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);cout<<fixed<<setprecision(15);
-  
+  int a; 
+  cin >> a;
+  cout << a << endl;
   return 0;
 }
